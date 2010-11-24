fd6472004ace42151520cf68562e358793bd44f4
Andrey Vasenin <avasenin@axmor.com>
2010-11-24 20:22:55 +0600

    new build files

--- bmmkern/src/build.sh
+++ bmmkern/src/build.sh
@@ -1,1 +1,4 @@
-gcc -O3 -mfpmath=sse -finline-limit=5000 -fno-strict-aliasing  -msse2 -lboost_filesystem -lboost_program_options -lboost_regex -lboost_thread -I/home/avasenin/diplom/bmmkern/include __moldefs.cpp bmmkern.cpp -obmmkern.exe
+CURRENT_DIR=`pwd`
+INCLUDE_DIR=$CURRENT_DIR/../include
+echo $INCLUDE_DIR
+gcc -O3 -mfpmath=sse -finline-limit=5000 -fno-strict-aliasing  -msse2 -lboost_filesystem -lboost_program_options -lboost_regex -lboost_thread -I$INCLUDE_DIR __moldefs.cpp bmmkern.cpp -o bmmkern.exe
